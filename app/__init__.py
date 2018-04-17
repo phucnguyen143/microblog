5d567a5e4c2604c18961dfac8dfbe58a4daad3b4
phucng143 <phuc.ng143@gmail.com>
2018-04-17 16:44:12 +0700

    update 170418

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
@@ -35,7 +37,7 @@ if not app.debug:
 		os.mkdir('logs')
 	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=1024,
 									   backupCount=10)
-	file_handler.setFormatter(loggin.Formatter(
+	file_handler.setFormatter(logging.Formatter(
 		'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 	file_handler.setLevel(logging.INFO)
 	app.logger.addHandler(file_handler)
